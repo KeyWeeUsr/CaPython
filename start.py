1cee46e5dcc432c646c1eaaf0ceebe1ebfb16785
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-10-06 21:53:57 +0200

    [ADD] Add CLI arguments for start.py

--- start.py
+++ start.py
@@ -1,8 +1,10 @@
 import sys
+from argparse import ArgumentParser, Namespace
 from subprocess import Popen
 
 
 def cmd(*args, **kwargs):
+    print(args, kwargs)
     should_skip = False
     if "skip" in kwargs:
         should_skip = kwargs.pop("skip")
@@ -13,14 +15,24 @@ def cmd(*args, **kwargs):
         sys.exit(proc.returncode)
 
 
+def get_parser() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--skip-camunda-kill", action="store_true")
+    return parser
+
+
 def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
     cmd(["which", "docker"])
-    cmd(["docker", "pull", "camunda/camunda-bpm-platform:latest"])
-    cmd(["docker", "rm", "-f", "camunda"], skip=True)
-    cmd([
-        "docker", "run", "-d", "--name", "camunda",
-        "-p", "127.0.0.1:8080:8080", "camunda/camunda-bpm-platform:latest"
-    ])
+    if not args.skip_camunda_kill:
+        cmd(["docker", "pull", "camunda/camunda-bpm-platform:latest"])
+        cmd(["docker", "rm", "-f", "camunda"], skip=True)
+        cmd([
+            "docker", "run", "-d", "--name", "camunda",
+            "-p", "127.0.0.1:8080:8080", "camunda/camunda-bpm-platform:latest"
+        ])
 
 
 if __name__ == "__main__":
