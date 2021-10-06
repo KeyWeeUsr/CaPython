2bc491e502e907376b102704a0b0fab668577d77
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-10-06 22:05:46 +0200

    [REF] Use docker-compose.yml instead of custom start.py

--- start.py
+++ start.py
@@ -16,12 +16,11 @@ def cmd(*args, **kwargs):
 
 
 def dexec(container, *args):
-    cmd(["docker", "exec", "-it", container, "sh", "-c", *args])
+    cmd(["docker-compose", "exec", container, "sh", "-c", *args])
 
 
 def get_parser() -> Namespace:
     parser = ArgumentParser()
-    parser.add_argument("-s", "--skip-camunda-kill", action="store_true")
     return parser
 
 
@@ -29,29 +28,7 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    cmd(["which", "docker"])
-
-    camunda_name = "camunda"
-    net_name = "camunda-network"
-    stdlib_name = "camunda-stdlib"
-
-    cmd(["docker", "network", "create"
-    if not args.skip_camunda_kill:
-        cmd(["docker", "pull", "camunda/camunda-bpm-platform:latest"])
-        cmd(["docker", "rm", "-f", camunda_name], skip=True)
-        cmd([
-            "docker", "run", "-d",
-            "--name", camunda_name,
-            "--network", net_name,
-            "-p", "127.0.0.1:8080:8080", "camunda/camunda-bpm-platform:latest"
-        ])
-
-    cmd(["docker", "rm", "-f", stdlib_name], skip=True)
-    cmd([
-        "docker", "run", "-d", "--name", stdlib_name,
-        "--network", net_name,
-        "-p", "127.0.0.1:9090:8080", "python:alpine"
-    ])
+    cmd(["docker-compose", "up", "-d"])
     dexec(stdlib_name, ["pip", "install", "-U", "pip"])
 
 
