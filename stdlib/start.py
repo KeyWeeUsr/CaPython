b7efc7e1324309831e0215703bfe6f07ddad51bd
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-10-09 18:06:50 +0200

    [ADD] Add handling for 3rd-party package installation before start

--- stdlib/start.py
+++ stdlib/start.py
@@ -1,4 +1,6 @@
 import sys
+from os import environ
+from os.path import exists, isfile
 from subprocess import Popen
 
 
@@ -13,6 +15,9 @@ def cmd(*args, **kwargs):
 def main():
     cmd(["pip", "install", "-U", "pip"])
     cmd(["pip", "install", "camunda-external-task-client-python3"])
+    reqs = environ.get("PYTHON_REQUIREMENTS", "/non-existing")
+    if exists(reqs) and isfile(reqs):
+        cmd(["pip", "install", "-U", "-r", reqs])
     cmd(["python", "-u", "/app/main.py"])
 
 
