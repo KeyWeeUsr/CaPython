ef4aca6ebc26c098a053f97f5a79318533b4439f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-10-11 00:07:38 +0200

    [ADD] Add env vars for better configuration

--- stdlib/main.py
+++ stdlib/main.py
@@ -1,6 +1,8 @@
 import json
 from enum import Enum
+from os import environ
 from traceback import format_exc
+from uuid import uuid4
 
 from camunda.external_task.external_task import ExternalTask, TaskResult
 from camunda.external_task.external_task_worker import ExternalTaskWorker
@@ -14,6 +16,18 @@ DEFAULT_CONFIG = {
     "retryTimeout": 5000,
     "sleepSeconds": 30
 }
+
+# config vars
+CAPYTHON_SCRIPT_ENTRYPOINT = environ.get("CAPYTHON_SCRIPT_ENTRYPOINT")
+CAPYTHON_BASE_URL = environ.get(
+    "CAPYTHON_BASE_URL", "http://camunda:8080/engine-rest"
+)
+CAPYTHON_TOPIC_SEPARATOR = ","
+CAPYTHON_TOPICS = environ.get(
+    "CAPYTHON_TOPICS".split(CAPYTHON_TOPIC_SEPARATOR), ["topic"]
+)
+CAPYTHON_ID = environ.get("CAPYTHON_ID", str(uuid4()))
+
 
 class Topic(Enum):
     topic = "topic"
@@ -47,7 +61,7 @@ def execute(task: ExternalTask):
         variables = task.get_variables()
         variables["BpmnException"] = BpmnException
 
-        exec(variables.get("python", "") or "", variables)
+        exec(variables.get(CAPYTHON_SCRIPT_ENTRYPOINT, "") or "", variables)
 
         # clean trash in globals
         clean_vars(variables)
@@ -82,7 +96,7 @@ def handler(task: ExternalTask) -> TaskResult:
 
 if __name__ == '__main__':
     worker = ExternalTaskWorker(
-        worker_id="1", base_url="http://camunda:8080/engine-rest",
+        worker_id=CAPYTHON_ID, base_url=CAPYTHON_BASE_URL,
         config=DEFAULT_CONFIG
     )
-    worker.subscribe(["topic"], handler)
+    worker.subscribe(CAPYTHON_TOPICS, handler)
