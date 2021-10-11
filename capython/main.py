384fdc5dd99620243303576f5ef5b4a7b70f18fe
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2021-10-11 02:15:37 +0200

    [REF] Rename to CaPython, add config options

--- capython/main.py
+++ capython/main.py
@@ -7,30 +7,36 @@ from uuid import uuid4
 from camunda.external_task.external_task import ExternalTask, TaskResult
 from camunda.external_task.external_task_worker import ExternalTaskWorker
 
-
-DEFAULT_CONFIG = {
-    "maxTasks": 1,
-    "lockDuration": 10000,
-    "asyncResponseTimeout": 5000,
-    "retries": 3,
-    "retryTimeout": 5000,
-    "sleepSeconds": 30
-}
-
 # config vars
-CAPYTHON_SCRIPT_ENTRYPOINT = environ.get("CAPYTHON_SCRIPT_ENTRYPOINT")
+CAPYTHON_SCRIPT_ENTRYPOINT = environ.get(
+    "CAPYTHON_SCRIPT_ENTRYPOINT", "capython"
+)
 CAPYTHON_BASE_URL = environ.get(
     "CAPYTHON_BASE_URL", "http://camunda:8080/engine-rest"
 )
 CAPYTHON_TOPIC_SEPARATOR = ","
 CAPYTHON_TOPICS = environ.get(
-    "CAPYTHON_TOPICS".split(CAPYTHON_TOPIC_SEPARATOR), ["topic"]
-)
+    "CAPYTHON_TOPICS", "topic"
+).split(CAPYTHON_TOPIC_SEPARATOR)
 CAPYTHON_ID = environ.get("CAPYTHON_ID", str(uuid4()))
 
+CAPYTHON_MAX_TASKS = int(environ.get("CAPYTHON_MAX_TASKS", 1))
+CAPYTHON_LOCK_DURATION = int(environ.get("CAPYTHON_LOCK_DURATION", 10000))
+CAPYTHON_ASYNC_RESPONSE_TIMEOUT = int(environ.get(
+    "CAPYTHON_ASYNC_RESPONSE_TIMEOUT", 5000
+))
+CAPYTHON_RETRIES = int(environ.get("CAPYTHON_RETRIES", 3))
+CAPYTHON_RETRY_TIMEOUT = int(environ.get("CAPYTHON_RETRY_TIMEOUT", 5000))
+CAPYTHON_SLEEP_SECONDS = int(environ.get("CAPYTHON_SLEEP_SECONDS", 30))
 
-class Topic(Enum):
-    topic = "topic"
+DEFAULT_CONFIG = {
+    "maxTasks": CAPYTHON_MAX_TASKS,
+    "lockDuration": CAPYTHON_LOCK_DURATION,
+    "asyncResponseTimeout": CAPYTHON_ASYNC_RESPONSE_TIMEOUT,
+    "retries": CAPYTHON_RETRIES,
+    "retryTimeout": CAPYTHON_RETRY_TIMEOUT,
+    "sleepSeconds": CAPYTHON_SLEEP_SECONDS
+}
 
 
 class BpmnException(Exception):
@@ -50,14 +56,29 @@ def prepare_vars(task):
     variables = task.get_variables()
     variables["BpmnException"] = BpmnException
     variables["__task__"] = task
+    variables["__task_id__"] = task.get_task_id()
+    variables["__topic__"] = task.get_topic_name()
     return variables
 
 
+def failure_recovery_opts(variables):
+    return dict(
+        max_retries=int(variables.get("__task_retries__") or 0),
+        retry_timeout=int(
+            variables.get("__task_retry_timeout__") or CAPYTHON_RETRY_TIMEOUT
+        )
+    )
+
+
 def clean_vars(variables):
-    if "__builtins__" in variables:
-        del variables["__builtins__"]
-        del variables["BpmnException"]
-        del variables["__task__"]
+    known = [
+        "__builtins__", "BpmnException", "__task__", "__task_id__",
+        "__topic__", "__task_retries__", "__task_retry_timeout__"
+    ]
+    for var in known:
+        if var not in variables:
+            continue
+        del variables[var]
 
 
 def serialize_vars(variables):
@@ -88,16 +109,12 @@ def execute(task: ExternalTask):
             )
         return task.failure(
             error_message="Python Exception",  error_details=formatted,
-            max_retries=0, retry_timeout=0
+            **failure_recovery_opts(variables)
         )
 
 
 def handler(task: ExternalTask) -> TaskResult:
-    topic = task.get_topic_name()
-    task_id = task.get_task_id()
-
-    if topic == Topic.topic.value:
-        return execute(task)
+    return execute(task)
 
 
 if __name__ == '__main__':
